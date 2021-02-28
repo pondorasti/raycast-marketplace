cdadbb5d958c5ef9af206f2c3547548625adf6e2
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-02-28 15:05:43 -0800

    Fix GitKraken hooks issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,7 +47,7 @@ export default function Home(): JSX.Element {
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
+          target='_blank'
           rel="noopener noreferrer"
         >
           Powered by <img src='/vercel.svg' alt="Vercel Logo" className={styles.logo} />
