430772edf1b25978e4d912cdc1fabd370caaf1b6
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-02-28 15:14:22 -0800

    Add eslint --fix to pre-commit hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,11 +46,11 @@ export default function Home(): JSX.Element {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
           target='_blank'
           rel="noopener noreferrer"
         >
-          Powered by <img src='/vercel.svg' alt="Vercel Logo" className={styles.logo} />
+          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
