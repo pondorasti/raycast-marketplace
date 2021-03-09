42cfba6308938505a3fa08df4a4017cc74199c16
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-03-08 17:22:04 -0800

    Update index page with build factories

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,46 @@
 import Head from "next/head"
+import { GetStaticProps, InferGetStaticPropsType } from "next"
 import CssBaseline from "@material-ui/core/CssBaseline"
-import { StylesProvider } from "@material-ui/core/"
-import TextField from "@material-ui/core/TextField"
+import { StylesProvider, Grid, Box, Typography } from "@material-ui/core/"
 import { ThemeProvider } from "@material-ui/core/styles"
-import RayTheme from "../components/RayTheme"
 
-export default function Home(): JSX.Element {
+import RayTheme from "../components/RayTheme"
+import { Command } from "../components/CommandCard"
+import CommandCard from "../components/CommandCard/CommandCard"
+
+interface CommandsGroup {
+  name: string
+  path: string
+  scriptCommands: Command[]
+  subGroups?: CommandsGroup[]
+}
+
+export default function Home({ commandsGroups }: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
+  // console.log(commands[0].subGroups[0].scriptCommands[0].title)
+
+  function commandsFactory(scriptCommands: Command[]): JSX.Element {
+    return (
+      <Grid container spacing={2}>
+        {scriptCommands.map((command: Command) => (
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          <CommandCard key={command.title} {...command} />
+        ))}
+      </Grid>
+    )
+  }
+
+  function groupsFactory(groups: CommandsGroup[], isSubGroup = false) {
+    return groups.map((group: CommandsGroup) => (
+      <div key={group.name}>
+        <Typography key={group.name} variant={isSubGroup ? "h6" : "h5"}>
+          {group.name}
+        </Typography>
+        {group.scriptCommands.length !== 0 && commandsFactory(group.scriptCommands)}
+        {group.subGroups && groupsFactory(group.subGroups, true)}
+      </div>
+    ))
+  }
+
   return (
     <StylesProvider injectFirst>
       <ThemeProvider theme={RayTheme}>
@@ -16,9 +51,22 @@ export default function Home(): JSX.Element {
         </Head>
         <main>
           <h1>hello</h1>
-          <TextField label="Language" />
+          <Box display="flex" flexDirection="column">
+            {groupsFactory(commandsGroups)}
+          </Box>
         </main>
       </ThemeProvider>
     </StylesProvider>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const api = "https://raw.githubusercontent.com/raycast/script-commands/master/commands/extensions.json"
+  const result = await fetch(api)
+  const data = await result.json()
+  const commandsGroups: CommandsGroup[] = data.groups
+
+  return {
+    props: { commandsGroups },
+  }
 }
