d80ec75bfbb1a77536c5321f4e4b5f79ba2e6709
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-03-14 01:45:56 -0700

    Add Fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
+import { useEffect } from "react"
 import { AppProps } from "next/app"
+import { useRouter } from "next/router"
+import * as Fathom from "fathom-client"
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  // Fathom
+  // Source: https://vercel.com/guides/deploying-nextjs-using-fathom-analytics-with-vercel
+  const router = useRouter()
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load("MGCVJOTW", {
+      includedDomains: ["raycast-marketplace.vercel.app"],
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete)
+    }
+  }, [])
+
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />
 }
