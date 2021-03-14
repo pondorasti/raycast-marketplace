ce352683cb66bc2592548463f74a4dd1eb19a70d
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-03-14 01:27:23 -0700

    Polish search bar implementation

--- components/CommandsGrid/CommandsGrid.tsx
+++ components/CommandsGrid/CommandsGrid.tsx
@@ -1,5 +1,6 @@
+import { useState, ChangeEvent } from "react"
 import Link from "next/link"
-import { Tab, Tabs, Box, Typography, Grid } from "@material-ui/core"
+import { Tab, Tabs, Box, Typography, Grid, TextField } from "@material-ui/core"
 import CommandsGroup from "./types"
 import { Command, CommandCard } from "../CommandCard"
 
@@ -16,19 +17,35 @@ export default function CommandsGrid({ commandsGroups }: ICommandsGrid): JSX.Ele
   ))
 
   // Grid
-  function commandsFactory(scriptCommands: Command[]): JSX.Element {
-    return (
-      <Grid container spacing={3}>
-        {scriptCommands.map((command: Command) => (
-          // eslint-disable-next-line react/jsx-props-no-spreading
-          <CommandCard key={command.title} {...command} />
-        ))}
-      </Grid>
-    )
+  function shouldHideCard(command: Command): boolean {
+    return command.title.toLowerCase().includes("shell")
   }
+
   function groupsFactory(groups: CommandsGroup[], isSubGroup = false) {
+    let hasAtLeastOneCard = false
+
+    // Inner factory for building the Grid of Cards
+    function commandsFactory(scriptCommands: Command[]): JSX.Element {
+      const cards = scriptCommands.map((command: Command) => {
+        const isHidden = shouldHideCard(command)
+
+        // Logic for detecting if at least one card is shown
+        hasAtLeastOneCard = hasAtLeastOneCard || !isHidden
+
+        // TODO: use props={command} after fixing null icon
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        return <CommandCard key={command.title} {...command} isHidden={isHidden} />
+      })
+
+      return (
+        <Grid container spacing={3}>
+          {cards}
+        </Grid>
+      )
+    }
+
     return groups.map((group: CommandsGroup) => (
-      <Box key={group.name} id={group.name}>
+      <Box key={group.name} id={group.name} display={hasAtLeastOneCard ? "block" : "none"}>
         <Typography
           key={group.name}
           variant={isSubGroup ? "h6" : "h5"}
@@ -40,21 +57,40 @@ export default function CommandsGrid({ commandsGroups }: ICommandsGrid): JSX.Ele
           {group.name}
         </Typography>
         {group.scriptCommands.length !== 0 && commandsFactory(group.scriptCommands)}
+
+        {/* Render subGroups */}
         {group.subGroups && groupsFactory(group.subGroups, true)}
       </Box>
     ))
   }
 
   const sidebarWidth = "200px"
+  const [searchQuery, setSearchQuery] = useState("")
+  const handleSearchQuery = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value)
+  }
 
   return (
     <>
-      <Box position="fixed" width={sidebarWidth}>
-        <Tabs orientation="vertical">{tabsHTML}</Tabs>
+      <Box
+        position="sticky"
+        top="0"
+        paddingTop={4}
+        paddingBottom={4}
+        zIndex={10}
+        width="100%"
+        bgcolor="background.default"
+      >
+        <TextField label="Search" value={searchQuery} onChange={handleSearchQuery} fullWidth />
       </Box>
 
-      <Box marginLeft={sidebarWidth} paddingLeft={2} display="flex" flexDirection="column">
-        {groupsFactory(commandsGroups)}
+      <Box display="flex">
+        <Box position="sticky" top="120px" height="fit-content" width={sidebarWidth}>
+          <Tabs orientation="vertical">{tabsHTML}</Tabs>
+        </Box>
+        <Box paddingLeft={2} display="flex" flexDirection="column">
+          {groupsFactory(commandsGroups)}
+        </Box>
       </Box>
     </>
   )
