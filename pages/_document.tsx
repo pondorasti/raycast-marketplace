f814a9abe86f9ec1002db801c098c2fc19f41a3a
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-04-17 22:55:43 -0700

    Add Inter Font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,10 @@ export default class Document extends NextDocument {
       <Html lang="en" className="bg-gray-50">
         <Head>
           <title>Raycast Marketplace</title>
+
           <link rel="icon" href="/favicon.ico" />
+
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         </Head>
         <body>
           <Main />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
