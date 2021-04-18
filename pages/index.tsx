64a3260d7d2436e08678b7c7daf4bae4f35c1fcc
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-04-18 14:17:18 -0700

    Add markdown parsing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import { GetStaticProps, InferGetStaticPropsType } from "next"
+import remark from "remark"
+import remarkHtml from "remark-html"
 import { CommandsGrid, CommandsGroup } from "../components/CommandsGrid"
 import HeroSection from "../components/HeroSection/HeroSection"
 
 export default function Home({ commandsGroups }: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
-  // console.log(commands[0].subGroups[0].scriptCommands[0].title)
-
   return (
     <main className="bg-gray-50">
       <HeroSection />
@@ -19,6 +19,23 @@ export const getStaticProps: GetStaticProps = async () => {
   const data = await result.json()
   const commandsGroups: CommandsGroup[] = data.groups
   commandsGroups.sort((a, b) => (a.name > b.name ? 1 : -1))
+
+  function remarkCommands(groups: CommandsGroup[]) {
+    groups.forEach(async (group) => {
+      group.scriptCommands.forEach(async (script) => {
+        const description = script.description || "N/A"
+        const descriptionHtml = (await remark().use(remarkHtml, { sanitize: true }).process(description)).toString()
+        // eslint-disable-next-line no-param-reassign
+        script.descriptionHtml = descriptionHtml
+      })
+
+      if (group.subGroups) {
+        remarkCommands(group.subGroups)
+      }
+    })
+  }
+
+  remarkCommands(commandsGroups)
 
   return {
     props: { commandsGroups },
