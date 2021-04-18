1f528137a918ede62006c7c6a227e304027530f5
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-04-17 22:45:17 -0700

    Add _document file

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import NextDocument, { Html, Head, Main, NextScript } from "next/document"
+
+export default class Document extends NextDocument {
+  render(): JSX.Element {
+    return (
+      <Html lang="en" className="bg-gray-50">
+        <Head>
+          <title>Raycast Marketplace</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <script> </script>
+        </body>
+      </Html>
+    )
+  }
+}
