c822e88177d87204e6677848fd5b865508815905
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-04-18 14:40:32 -0700

    Improved typography styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,23 @@ module.exports = {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: [
+            {
+              a: {
+                color: theme("colors.gray.500"),
+              },
+              code: {
+                color: theme("colors.gray.500"),
+              },
+              "a code": {
+                color: theme("colors.gray.500"),
+              },
+            },
+          ],
+        },
+      }),
     },
   },
   variants: {
