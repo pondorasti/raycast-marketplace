c7bc69bb0b00e26327043d522aa34d17d80a33bc
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-04-23 23:30:28 -0700

    WIP Header

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,13 +6,26 @@ export default class Document extends NextDocument {
       <Html lang="en" className="bg-gray-50">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         </Head>
-        <body className="max-w-7xl mx-auto px-4 sm:px-6 xl:px-8">
-          <Main />
-          <NextScript />
-          <script> </script>
+        <body className="max-w-7xl mx-auto">
+          <header className="sticky top-0 z-30 mx-auto bg-opacity-50 h-[72px] bg-gray-50 backdrop-filter backdrop-blur max-w-8xl xl:px-8 firefox:bg-opacity-90">
+            <div className="flex items-center justify-between py-5 px-4 sm:px-6 xl:px-8 border-b border-gray-800">
+              <a className="block" href="/react">
+                hello
+              </a>
+              <a className="text-gray-400 hover:text-white" href="https://github.com/tailwindlabs/headlessui">
+                <span className="sr-only">GitHub repository</span>
+                <svg viewBox="0 0 16 16" fill="currentColor" className="w-5 h-5">
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
+                </svg>
+              </a>
+            </div>
+          </header>
+          <div className="px-4 sm:px-6 xl:px-8">
+            <Main />
+            <NextScript />
+          </div>
         </body>
       </Html>
     )
