d20940ed196ec0084b8fe6898c47b0443fca4659
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-04-27 11:52:36 -0700

    Add Favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,11 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en" className="nightwind">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         </Head>
         <body className="max-w-7xl mx-auto bg-gray-50 dark:bg-darkGray-600">
