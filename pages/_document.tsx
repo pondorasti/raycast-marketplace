4fe64e611c5d7ed7eee485cc62cae64e654fd030
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-04-26 23:24:04 -0700

    Add dark mode toggle

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,16 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document"
-import nightwind from "nightwind/helper"
-import { horizontalPadding } from "@utils/styles"
-import NavigationBar from "@components/NavigationBar"
 
 export default class Document extends NextDocument {
   render(): JSX.Element {
     return (
-      <Html lang="en">
+      <Html lang="en" className="nightwind">
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-          {/* eslint-disable-next-line react/no-danger */}
-          <script dangerouslySetInnerHTML={{ __html: nightwind.init() }} />
         </Head>
         <body className="max-w-7xl mx-auto bg-gray-50 dark:bg-darkGray-600">
-          <NavigationBar />
-          <div className={horizontalPadding}>
-            <Main />
-            <NextScript />
-          </div>
+          <Main />
+          <NextScript />
         </body>
       </Html>
     )
