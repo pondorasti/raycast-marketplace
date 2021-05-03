11ba779867c1605eac754c4102fa2f5b0c63d92b
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-05-02 18:11:55 -0700

    Improve HeroSection styling

--- components/HeroSection/HeroSection.tsx
+++ components/HeroSection/HeroSection.tsx
@@ -1,6 +1,6 @@
 export default function HeroSection(): JSX.Element {
   return (
-    <div className="relative overflow-y-hidden">
+    <div className="relative overflow-hidden">
       <div className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full" aria-hidden="true">
         <div className="relative h-full max-w-7xl mx-auto">
           <svg
@@ -25,11 +25,11 @@ export default function HeroSection(): JSX.Element {
             <rect width={404} height={784} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
           </svg>
           <svg
-            className="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
+            className="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-2/3 lg:-translate-x-1/2"
             width={404}
-            height={784}
+            height={770}
             fill="none"
-            viewBox="0 0 404 784"
+            viewBox="0 0 404 770"
           >
             <defs>
               <pattern
@@ -43,14 +43,14 @@ export default function HeroSection(): JSX.Element {
                 <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
               </pattern>
             </defs>
-            <rect width={404} height={784} fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)" />
+            <rect width={404} height={770} fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)" />
           </svg>
         </div>
       </div>
 
-      <div className="my-16 mx-auto max-w-7xl px-4 sm:my-24 z-10">
+      <div className="relative my-16 mx-auto max-w-7xl px-4 sm:my-24 z-10">
         <div className="text-center">
-          <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+          <h1 className="text-4xl tracking-tight font-bold text-gray-900 sm:text-5xl">
             <span
               className="block text-ray"
               style={{
