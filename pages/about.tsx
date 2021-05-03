0372d88c0b0bcd5e6ed41e4e8f43d931a0d80e4f
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-05-03 00:00:08 -0700

    Add about page

---
+++ pages/about.tsx
@@ -0,0 +1,41 @@
+import { GetStaticProps, InferGetStaticPropsType } from "next"
+import Head from "next/head"
+import remark from "remark"
+import remarkHtml from "remark-html"
+import remarkExternalLinks from "remark-external-links"
+import gfm from "remark-gfm"
+
+export default function Home({ markdown }: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Raycast Marketplace - About</title>
+      </Head>
+      <main>
+        <div
+          className="mx-auto prose"
+          /* eslint-disable-next-line react/no-danger */
+          dangerouslySetInnerHTML={{ __html: markdown }}
+        />
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const api = "https://raw.githubusercontent.com/raycast/script-commands/master/README.md"
+  const result = await fetch(api)
+  const data = await result.text()
+
+  const markdown = (
+    await remark()
+      .use(remarkExternalLinks, { target: "_blank", rel: ["noopener", "noreferrer"] })
+      .use(remarkHtml)
+      .use(gfm)
+      .process(data)
+  ).toString()
+
+  return {
+    props: { markdown },
+  }
+}
