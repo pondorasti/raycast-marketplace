8c070f9e8ca27663ba13dba31d45b58fa28a37d3
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-07-14 21:32:57 -0700

    RAY 39: Improve search bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -81,7 +81,7 @@ module.exports = {
     require("@tailwindcss/forms"),
     require("nightwind"),
     require("@tailwindcss/line-clamp"),
-    plugin(function({ addUtilities }) {
+    plugin(function ({ addUtilities }) {
       addUtilities({
         ".h-navbar": {
           height: "69px",
@@ -90,7 +90,7 @@ module.exports = {
           top: "69px",
         },
         ".navbar-link": {
-          "@apply text-sm font-medium text-black opacity-70 hover:opacity-100": {}
+          "@apply text-sm font-medium text-black opacity-70 hover:opacity-100": {},
         },
         ".bg-blur": {
           "@apply bg-opacity-50 bg-gray-50 dark:bg-darkGray-600 dark:bg-opacity-50 backdrop-filter backdrop-blur": {},
@@ -98,7 +98,11 @@ module.exports = {
         ".px-body": {
           "@apply px-4 sm:px-6 xl:px-8": {},
         },
+        kbd: {
+          "@apply inline-flex items-center border border-gray-200 rounded px-2 text-sm font-sans font-medium text-gray-400":
+            {},
+        },
       })
-    })
+    }),
   ],
 }
